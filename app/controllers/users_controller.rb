6c70b49d0bcbf676557394b360ad98ddf38e7d27
Alyssa Yates <bosslys@icloud.com>
2019-11-25 18:04:37 +0000

    sign up fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,32 +5,13 @@ class UsersController < ApplicationController
     end
 
     post "/signup" do
-      @user = User.new
-      @user.name = params[:name]
-      @user.email = params[:email]
-      @user.password = params[:password]
-   if params[:user].values.any?{|attribute| attribute == ""}
-     redirect "/signup"
-   else
-     user = User.new
-     user.save
-     session[:id] = user.id
-     erb :"sessions/login.html"
+     if params[:user].values.any?{|attribute| attribute == ""}
+       redirect "/signup"
+     else
+       @user = User.new(params[:user])
+       @user.save
+       session[:id] = @user.id
+       erb :"sessions/login.html"
+     end
    end
- end
-
- post '/users' do
-   @user = User.new
-   @user.email = params[:email]
-   @user.password = params[:password]
-   if @user.save
-     erb :"sessions/login.html"
-   else
-     erb :'users/signup.html'
-   end
- end
-
- post '/sessions' do
-   erb :"posts/new"
-  end
 end
