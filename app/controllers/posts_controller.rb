bda54c3b4a455ad99ae5ef672e7ce392193dd4d9
Alyssa Yates <bosslys@icloud.com>
2019-11-25 19:34:28 +0000

    after project review

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
       erb :"posts/index"
     else
       redirect "/login"
-    end 
+    end
   end
 
   get '/user/posts' do
@@ -15,13 +15,30 @@ class PostsController < ApplicationController
     erb :"posts/index"
   end
 
+ #  post "/signup" do
+ #   if params[:user].values.any?{|attribute| attribute == ""}
+ #     redirect "/signup"
+ #   else
+ #     @user = User.new(params[:user])
+ #     @user.save
+ #     session[:id] = @user.id
+ #     erb :"sessions/login.html"
+ #   end
+ # end
+
   get '/posts/new' do
       erb :"posts/new"
   end
 
   post '/posts' do
-    @post = current_user.posts.create(:title => params[:title], :content => params[:content])
-    redirect to "/posts"
+    binding.pry
+    @post = current_user.posts.build(:title => params[:title], :content => params[:content])
+    if @post(:title => params[:title], :content => params[:content]).any?{|attribute| attribute == ""}
+      redirect "/posts/new"
+    else
+      @post.save
+      redirect to "/posts"
+    end
   end
 
 
